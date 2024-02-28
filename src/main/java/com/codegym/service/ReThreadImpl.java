bd8700e01ba64e3bcd9c69adcac9c7b10e76d4d0
phunglam98 <duclam9812@gmail.com>
2024-02-28 19:55:32 +0700

    lam lol

--- src/main/java/com/codegym/service/ReThreadImpl.java
+++ src/main/java/com/codegym/service/ReThreadImpl.java
@@ -2,29 +2,35 @@ package com.codegym.service;
 
 import com.codegym.model.ReThread;
 import com.codegym.service.impl.ReThreadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class ReThreadImpl implements ReThreadService {
+    @Autowired
+    private ReThreadService reThreadService;
+
     @Override
     public void save(ReThread reThread) {
-
+        reThreadService.save(reThread);
     }
 
     @Override
     public void delete(Long id) {
+        reThreadService.delete(id);
 
     }
 
     @Override
     public List<ReThread> findAll() {
-        return null;
+        return reThreadService.findAll();
     }
 
     @Override
     public Optional<ReThread> findEById(Long id) {
-        return Optional.empty();
+        return reThreadService.findEById(id);
     }
 }
--- src/main/java/com/codegym/service/UsedImageImpl.java
+++ src/main/java/com/codegym/service/UsedImageImpl.java
@@ -2,29 +2,34 @@ package com.codegym.service;
 
 import com.codegym.model.UsedImage;
 import com.codegym.service.impl.UsedImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 @Service
 public class UsedImageImpl implements UsedImageService {
+    @Autowired
+    private UsedImageService usedImageService;
     @Override
     public void save(UsedImage usedImage) {
+        usedImageService.save(usedImage);
 
     }
 
     @Override
     public void delete(Long id) {
+        usedImageService.delete(id);
 
     }
 
     @Override
     public List<UsedImage> findAll() {
-        return null;
+        return usedImageService.findAll();
     }
 
     @Override
     public Optional<UsedImage> findEById(Long id) {
-        return Optional.empty();
+        return usedImageService.findEById(id);
     }
 }
